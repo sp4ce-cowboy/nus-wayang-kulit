3a9aa79104d242aad4226a987fed2eb048c2cd44
Rubesh <rubesh77@gmail.com>
2024-10-18 13:58:55 +0800

    Update puppet part 3d to account for offset

--- src/models/PuppetPart3D.js
+++ src/models/PuppetPart3D.js
@@ -2,10 +2,83 @@ import * as THREE from 'three';
 import { OBJLoader } from 'three-stdlib';
 import { MTLLoader } from 'three-stdlib';
 
+export class PuppetPart3D {
+    constructor(objPath, mtlPath) {
+        this.objPath = objPath;
+        this.mtlPath = mtlPath;
+        this.mtlLoader = new MTLLoader();
+        this.objLoader = new OBJLoader();
+
+        this.group = new THREE.Group();  // Group to act as the pivot point
+        this.mesh = null;
+
+        this.loadModel(() => {
+            this.onReady(); // Trigger the callback when model is ready
+        });
+    }
+
+    loadModel(callback) {
+        this.mtlLoader.load(
+            this.mtlPath,
+            (materials) => {
+                materials.preload();
+                this.objLoader.setMaterials(materials);
+
+                this.objLoader.load(
+                    this.objPath,
+                    (object) => {
+                        this.mesh = object;
+
+                        // Center the mesh within the group
+                        this.mesh.position.set(this.mesh.position.x, this.mesh.position.y, this.mesh.position.z);
+
+                        // Add the mesh to the group (pivot)
+                        this.group.add(this.mesh);
+
+                        callback(); // Call the callback after the model is loaded
+                    },
+                    undefined,
+                    (error) => {
+                        console.error('Error loading the OBJ model:', error);
+                    }
+                );
+            },
+            undefined,
+            (error) => {
+                console.error('Error loading the MTL file:', error);
+            }
+        );
+    }
+
+    addToScene(scene) {
+        scene.add(this.group);  // Add the group (pivot) to the scene
+    }
+
+    setPosition(x, y, z) {
+        this.group.position.set(x, y, z);  // Adjust position of the group
+    }
+
+    setRotation(x, y, z) {
+        this.group.rotation.set(x, y, z);  // Adjust rotation of the group
+    }
+
+    move(x, y, z) {
+        this.group.position.x += x;
+        this.group.position.y += y;
+        this.group.position.z += z;
+    }
+
+    rotate(x, y, z) {
+        this.group.rotation.x += x;
+        this.group.rotation.y += y;
+        this.group.rotation.z += z;
+    }
+}
+
 /**
  * PuppetPart class to load and display 3D objects with materials.
  */
-export class PuppetPart3D {
+/*export class PuppetPart3D {
     constructor(objPath, mtlPath) {
         this.objPath = objPath;
         this.mtlPath = mtlPath;
@@ -82,3 +155,4 @@ export class PuppetPart3D {
         }
     }
 }
+*/
