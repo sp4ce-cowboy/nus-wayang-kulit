020af15344f83679217479a7e5c1606310159661
Rubesh <rubesh77@gmail.com>
2024-11-06 15:26:14 +0800

    Add deployment base

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,6 +1,9 @@
-export default {
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/nus-wayang-kulit/', // Replace with your GitHub repo name
   server: {
     host: '0.0.0.0', // Listen on all available network interfaces
-    port: 5174 // or your specified port
-  }
-}
+    port: 5174 // Specify your port here
+  },
+});
